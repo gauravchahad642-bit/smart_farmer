146ce31fc82f4ffad34576070481fc14abf0fddf
Gaurav Chahad <gauravchahad123@gmail.com>
2026-08-11 17:34:20 +0530

    Add python-jose

--- smart-farmer-frontend/src/api.js
+++ smart-farmer-frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
-      ? "http://127.0.0.1:8000"
+      ? "http://10.117.56.56:8000"
       : "https://smart-farmer-34kl.onrender.com",
 });
 

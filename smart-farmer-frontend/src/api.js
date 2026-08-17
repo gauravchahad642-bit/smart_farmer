e692b2a46e1b3cb151a49b875173e02f0b5fde5f
Gaurav Chahad <gauravchahad123@gmail.com>
2026-08-17 23:57:28 +0530

    Add python-jose

--- smart-farmer-frontend/src/api.js
+++ smart-farmer-frontend/src/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `http://${window.location.hostname}:8000`,
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://10.248.31.56:8000"
+      : "https://smart-farmer-34kl.onrender.com",
 });
 
 export default API;
